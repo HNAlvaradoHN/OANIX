f3af4e7bd5c35e15e43cee39c1aeaacd017958f3
HNAlvaradoHN <150305232+HNAlvaradoHN@users.noreply.github.com>
2026-08-16 22:07:55 -0600

    V3: register OANIX Keystore plugin

--- android/app/src/main/java/io/github/hnalvaradohn/oanix/MainActivity.java
+++ android/app/src/main/java/io/github/hnalvaradohn/oanix/MainActivity.java
@@ -1,5 +1,13 @@
 package io.github.hnalvaradohn.oanix;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        registerPlugin(OanixKeystorePlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
