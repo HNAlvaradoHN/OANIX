c65eca9c874bcb0609786676c83370cc8a305514
HNAlvaradoHN <150305232+HNAlvaradoHN@users.noreply.github.com>
2026-08-16 21:36:06 -0600

    V3: add Capacitor app configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'io.github.hnalvaradohn.oanix',
+  appName: 'OANIX',
+  webDir: 'dist',
+}
+
+export default config
