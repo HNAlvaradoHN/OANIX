f95d71c11c47794227ee98e0ba32a2507701947a
HNAlvaradoHN <150305232+HNAlvaradoHN@users.noreply.github.com>
2026-08-27 19:42:24 -0600

    refactor: remove eager legacy workspace imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,7 @@ import { useEffect, useRef, useState } from 'react'
 import { createPortal } from 'react-dom'
 import { VaultGate } from './VaultGate'
 import { WorkspaceRuntimeGate } from './WorkspaceRuntimeGate'
-import { WORKSPACE_V2_ENABLED } from './workspaceExperience'
 import { NotesWorkspace } from '../features/notes/NotesWorkspace'
-import { NoteListReorderGestureRuntime } from '../features/notes/NoteListReorderGestureRuntime'
-import { FolderGridRuntime } from '../features/folders/FolderGridRuntime'
 import { AccountPanel } from '../features/account/AccountPanel'
 import { AutoSyncRuntime } from '../features/sync/AutoSyncRuntime'
 import { ConflictCenter } from '../features/sync/ConflictCenter'
@@ -95,10 +92,8 @@ function UnlockedApp({ lockVault }: { lockVault: () => void }) {
       <NativeNoteShareRuntime />
       <AndroidKeystoreDiagnosticRuntime />
       <NotesWorkspace key={workspaceRevision} onLock={lockVault} />
-      <WorkspaceRuntimeGate />
-      {!WORKSPACE_V2_ENABLED && <NoteListReorderGestureRuntime key={`note-reorder-${workspaceRevision}`} />}
+      <WorkspaceRuntimeGate workspaceRevision={workspaceRevision} />
       <NoteAttachmentsRuntime key={`attachments-${workspaceRevision}`} />
-      {!WORKSPACE_V2_ENABLED && <FolderGridRuntime />}
       <NotePrivacyRuntime key={`privacy-${workspaceRevision}-${privacyRevision}`} />
       <NoteBulkPrivacyRuntime key={`privacy-bulk-${workspaceRevision}`} />
       <PrivateBoxListHint key={`private-hint-${workspaceRevision}`} />
