56193165df409116c119e39a9981654df7ab9764
HNAlvaradoHN <150305232+HNAlvaradoHN@users.noreply.github.com>
2026-08-27 19:09:09 -0600

    refactor: isolate workspace v2 order persistence

---
+++ src/features/notes/workspaceV2OrderService.ts
@@ -0,0 +1,18 @@
+import { persistFolderOrder } from '../folders/folderService'
+import { persistTagOrder } from '../tags/tagService'
+import { persistNoteOrder } from './noteService'
+import type { FolderRecord } from '../folders/folderTypes'
+import type { TagRecord } from '../tags/tagTypes'
+import type { NoteRecord } from './noteTypes'
+
+export function saveWorkspaceV2FolderOrder(ids: string[]): Promise<FolderRecord[]> {
+  return persistFolderOrder(ids)
+}
+
+export function saveWorkspaceV2TagOrder(ids: string[]): Promise<TagRecord[]> {
+  return persistTagOrder(ids)
+}
+
+export function saveWorkspaceV2NoteOrder(ids: string[]): Promise<NoteRecord[]> {
+  return persistNoteOrder(ids)
+}
