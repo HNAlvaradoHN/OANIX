3e47beeb1a5c954b462a7e3b3c6f0951ad558c9c
HNAlvaradoHN <150305232+HNAlvaradoHN@users.noreply.github.com>
2026-08-27 19:42:15 -0600

    refactor: lazy-load legacy workspace runtimes

--- src/app/WorkspaceRuntimeGate.tsx
+++ src/app/WorkspaceRuntimeGate.tsx
@@ -1,46 +1,33 @@
+import { lazy, Suspense } from 'react'
 import { EditorOperationRuntime } from '../features/editor/EditorOperationRuntime'
 import '../features/editor/editorTrailingWorkspace.css'
-import { FolderCreationRuntime } from '../features/folders/FolderCreationRuntime'
-import { FolderMobileDragRuntime } from '../features/folders/FolderMobileDragRuntime'
-import { FolderOperationFeedbackRuntime } from '../features/folders/FolderOperationFeedbackRuntime'
-import { FolderScopedManagerRuntime } from '../features/folders/FolderScopedManagerRuntime'
-import '../features/notes/folderDockContract.css'
 import { NoteCreationFeedbackRuntime } from '../features/notes/NoteCreationFeedbackRuntime'
-import { NoteMenuScrollDismiss } from '../features/notes/NoteMenuScrollDismiss'
-import { NoteMenuViewportFit } from '../features/notes/NoteMenuViewportFit'
-import { NoteVisualIdentityRuntime } from '../features/notes/NoteVisualIdentityRuntime'
-import { OrganicWorkspaceRuntime } from '../features/notes/OrganicWorkspaceRuntime'
-import { V383WorkspaceVisualRuntime } from '../features/notes/V383WorkspaceVisualRuntime'
-import { WorkspaceInputCompatibilityRuntime } from '../features/notes/WorkspaceInputCompatibilityRuntime'
-import { WorkspacePersonalizationRuntime } from '../features/notes/WorkspacePersonalizationRuntime'
 import '../features/privacy/noteBulkPrivacyOverrides.css'
 import { PrivacyStatusHelp } from '../features/privacy/PrivacyStatusHelp'
-import { TagMobileGestureRuntime } from '../features/tags/TagMobileGestureRuntime'
-import { WorkspaceQuickPolishRuntime } from './WorkspaceQuickPolishRuntime'
 import { WORKSPACE_V2_ENABLED } from './workspaceExperience'
 
-export function WorkspaceRuntimeGate() {
+const LegacyWorkspaceRuntimeGate = lazy(() =>
+  import('./LegacyWorkspaceRuntimeGate').then((module) => ({
+    default: module.LegacyWorkspaceRuntimeGate,
+  })),
+)
+
+interface WorkspaceRuntimeGateProps {
+  workspaceRevision: number
+}
+
+export function WorkspaceRuntimeGate({
+  workspaceRevision,
+}: WorkspaceRuntimeGateProps) {
   return (
     <>
       <EditorOperationRuntime />
       <NoteCreationFeedbackRuntime />
       <PrivacyStatusHelp />
       {!WORKSPACE_V2_ENABLED && (
-        <>
-          <FolderScopedManagerRuntime />
-          <OrganicWorkspaceRuntime />
-          <WorkspacePersonalizationRuntime />
-          <NoteVisualIdentityRuntime />
-          <NoteMenuScrollDismiss />
-          <NoteMenuViewportFit />
-          <WorkspaceInputCompatibilityRuntime />
-          <WorkspaceQuickPolishRuntime />
-          <FolderOperationFeedbackRuntime />
-          <FolderCreationRuntime />
-          <FolderMobileDragRuntime />
-          <TagMobileGestureRuntime />
-          <V383WorkspaceVisualRuntime />
-        </>
+        <Suspense fallback={null}>
+          <LegacyWorkspaceRuntimeGate workspaceRevision={workspaceRevision} />
+        </Suspense>
       )}
     </>
   )
