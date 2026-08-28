2048e218e3ecf628eeb6fc543026a2cd54f6d405
HNAlvaradoHN <150305232+HNAlvaradoHN@users.noreply.github.com>
2026-08-27 19:42:07 -0600

    refactor: isolate legacy workspace css behind lazy boundary

---
+++ src/app/LegacyWorkspaceRuntimeGate.tsx
@@ -0,0 +1,44 @@
+import { FolderCreationRuntime } from '../features/folders/FolderCreationRuntime'
+import { FolderGridRuntime } from '../features/folders/FolderGridRuntime'
+import { FolderMobileDragRuntime } from '../features/folders/FolderMobileDragRuntime'
+import { FolderOperationFeedbackRuntime } from '../features/folders/FolderOperationFeedbackRuntime'
+import { FolderScopedManagerRuntime } from '../features/folders/FolderScopedManagerRuntime'
+import '../features/notes/folderDockContract.css'
+import { NoteListReorderGestureRuntime } from '../features/notes/NoteListReorderGestureRuntime'
+import { NoteMenuScrollDismiss } from '../features/notes/NoteMenuScrollDismiss'
+import { NoteMenuViewportFit } from '../features/notes/NoteMenuViewportFit'
+import { NoteVisualIdentityRuntime } from '../features/notes/NoteVisualIdentityRuntime'
+import { OrganicWorkspaceRuntime } from '../features/notes/OrganicWorkspaceRuntime'
+import { V383WorkspaceVisualRuntime } from '../features/notes/V383WorkspaceVisualRuntime'
+import { WorkspaceInputCompatibilityRuntime } from '../features/notes/WorkspaceInputCompatibilityRuntime'
+import { WorkspacePersonalizationRuntime } from '../features/notes/WorkspacePersonalizationRuntime'
+import { TagMobileGestureRuntime } from '../features/tags/TagMobileGestureRuntime'
+import { WorkspaceQuickPolishRuntime } from './WorkspaceQuickPolishRuntime'
+
+interface LegacyWorkspaceRuntimeGateProps {
+  workspaceRevision: number
+}
+
+export function LegacyWorkspaceRuntimeGate({
+  workspaceRevision,
+}: LegacyWorkspaceRuntimeGateProps) {
+  return (
+    <>
+      <FolderScopedManagerRuntime />
+      <OrganicWorkspaceRuntime />
+      <WorkspacePersonalizationRuntime />
+      <NoteVisualIdentityRuntime />
+      <NoteMenuScrollDismiss />
+      <NoteMenuViewportFit />
+      <WorkspaceInputCompatibilityRuntime />
+      <WorkspaceQuickPolishRuntime />
+      <FolderOperationFeedbackRuntime />
+      <FolderCreationRuntime />
+      <FolderMobileDragRuntime />
+      <TagMobileGestureRuntime />
+      <V383WorkspaceVisualRuntime />
+      <NoteListReorderGestureRuntime key={`note-reorder-${workspaceRevision}`} />
+      <FolderGridRuntime />
+    </>
+  )
+}
